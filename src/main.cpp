fb432eb344bd33b9eee23a325c6c4fb67fd0541b
Dawid Jarzabek <248991@student.pwr.edu.pl>
2021-10-19 08:27:47 -0700

    Druga wtyczka

---
+++ src/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <dlfcn.h>
+#include <cassert>
+#include "Interp4Command.hh"
+#include "MobileObj.hh"
+
+using namespace std;
+
+
+int main()
+{
+  void *pLibHnd_Move = dlopen("libInterp4Move.so",RTLD_LAZY);
+  Interp4Command *(*pCreateCmd_Move)(void);
+  void *pFun;
+
+  if (!pLibHnd_Move) {
+    cerr << "!!! Brak biblioteki: Interp4Move.so" << endl;
+    return 1;
+  }
+
+
+  pFun = dlsym(pLibHnd_Move,"CreateCmd");
+  if (!pFun) {
+    cerr << "!!! Nie znaleziono funkcji CreateCmd" << endl;
+    return 1;
+  }
+  pCreateCmd_Move = *reinterpret_cast<Interp4Command* (**)(void)>(&pFun);
+
+
+  Interp4Command *pCmd = pCreateCmd_Move();
+
+  cout << endl;
+  cout << pCmd->GetCmdName() << endl;
+  cout << endl;
+  pCmd->PrintSyntax();
+  cout << endl;
+  pCmd->PrintCmd();
+  cout << endl;
+  
+  delete pCmd;
+
+  dlclose(pLibHnd_Move);
+
+ void *pLibHnd_Set = dlopen("libInterp4Set.so",RTLD_LAZY);
+  Interp4Command *(*pCreateCmd_Set)(void);
+
+  if (!pLibHnd_Set) {
+    cerr << "!!! Brak biblioteki: Interp4Set.so" << endl;
+    return 1;
+  }
+
+
+  pFun = dlsym(pLibHnd_Set,"CreateCmd");
+  if (!pFun) {
+    cerr << "!!! Nie znaleziono funkcji CreateCmd" << endl;
+    return 1;
+  }
+  pCreateCmd_Set = *reinterpret_cast<Interp4Command* (**)(void)>(&pFun);
+
+
+  pCmd = pCreateCmd_Set();
+
+  cout << endl;
+  cout << pCmd->GetCmdName() << endl;
+  cout << endl;
+  pCmd->PrintSyntax();
+  cout << endl;
+  pCmd->PrintCmd();
+  cout << endl;
+  
+  delete pCmd;
+
+  dlclose(pLibHnd_Set);
+}
